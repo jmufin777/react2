b4c4ce45a8c6e06a936243ce617cf988653f0c51
jmufin777 <jmufin777@gmail.com>
2024-01-21 13:20:13 +0100

    final-3

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -11,6 +11,12 @@ import { Suspense } from 'react';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Overview',
+};
+
 
  
 export default async function Page() {
--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,8 @@
-
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default async function Page() {
     return (
         <p>Customers</p>
